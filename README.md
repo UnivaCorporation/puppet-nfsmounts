0ad54aa41ed2601eeb6851e8b24487f15e8d3e9b
Damien Tougas <dtougas@univa.com>
2021-04-12 13:42:24 -0400

    Added the ability to set ownership/perms on both the mount point and mounted data

--- README.md
+++ README.md
@@ -35,3 +35,25 @@ nfsmounts::mounts:
     fstype: nfs
     options: defaults,ro
 ```
+
+Optional attributes can be added to set the ownership/permisisons on the mount point, and perform a one-time (recursive) setting of the ownership/permissions of the mounted files. This can be done by adding one or more of the following attributes to the previous example:
+
+```
+classes:
+  - nfsmounts
+
+nfsmounts::mounts:
+  '/tmp/tasty/yummy':
+    user: tasty-user
+    group: yummy-group
+    perms: 0755
+    data_user: tasty-user
+    data_group: yummy-group
+    data_perms: 0755
+    device: hostname-that-doesnt-exist.notadomain:/really/not/a/path
+    ensure: unmounted
+    fstype: nfs
+    options: defaults,ro
+```
+
+The setting of data ownership will create an empty flag file called `.data_owner_complete` at the root of the filesystem. The setting of data perms will create an empty file called `.data_perms_complete` at the root of the mounted filesystem. If none of the `data_*` options were specified, these files will not be created. Otherwise, the presence of these files ensures that the chown/chmod operations are only completed once. If you would like to have the owernship or perms set again, simply delete the appropriate flag file and trigger a run of puppet agent. 
--- manifests/init.pp
+++ manifests/init.pp
@@ -20,16 +20,98 @@ class nfsmounts(
   # might (not!) require separate installation of
   # https://forge.puppet.com/puppetlabs/mount_core/readme
   $mounts.each |$dir, $opts| {
+    # Owner of the mount point
+    if has_key($opts, "mount_user") {
+      $user = $opts["mount_user"]
+      $opts.delete("mount_user")
+    } else {
+      $user = "root"
+    }
+
+    # Owner of the files in the mounted filesystem
+    if has_key($opts, "data_user") {
+      $data_user = $opts["data_user"]
+      $opts.delete("data_user")
+    } else {
+      $date_user = ""
+    }
+
+    # Group of the mount point
+    if has_key($opts, "mount_group") {
+      $group = $opts["mount_group"]
+      $opts.delete("mount_group")
+    } else {
+      $group = "root"
+    }
+
+    # Group of the files in the moutned filesystem
+    if has_key($opts, "data_group") {
+      $data_group = $opts["data_group"]
+      $opts.delete("data_group")
+    } else {
+      $data_group = ""
+    }
+
+    # Perms of the mount point
+    if has_key($opts, "mount_perms") {
+      $perms = $opts["mount_perms"]
+      $opts.delete("mount_perms")
+    } else {
+      $perms = "0755"
+    }
+
+    # Perms on the files in the mounted filesystem
+    if has_key($opts, "data_perms") {
+      $data_perms = $opts["data_perms"]
+      $opts.delete("data_perms")
+    } else {
+      $data_perms = "0755"
+    }
+
+    # Create the directory, recursively
     exec { "create ${dir}":
-      user    => root,
-      command => "mkdir -m 0755 -p ${dir}",
+      user    => "root",
+      command => "mkdir -p ${dir}",
       path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
       creates => $dir,
     }
 
+    # Set the ownership and perms on the directory
+    file { $dir:
+      ensure => directory,
+      owner  => $user,
+      group  => $group,
+      mode   => $perms,
+      require => Exec["create ${dir}"],
+    }
+
+    # Mount the filesystem
     mount { $dir:
       * => $opts
     }
+
+    # Set the data ownership if required
+    if $data_user != "" or $data_group != "" {
+      exec { "set ${dir} owner":
+        user    => "root",
+        command => "chown -R ${data_user}:${data_group} ${dir}/* && touch ${dir}/.data_owner_complete",
+        path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
+        creates => "${dir}/.data_owner_complete",
+        require => Mount[$dir],
+      }
+    }
+
+    # Set the data perms if required
+    if $data_perms != "" {
+      exec { "set ${dir} perms":
+        user    => "root",
+        command => "chmod -R ${data_perms} ${dir}/* && touch ${dir}/.data_perms_complete",
+        path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
+        creates => "${dir}/.data_ownership_complete",
+        require => Mount[$dir],
+      }
+    }
+
   }
 
   ensure_packages([$mountpoint_pkg])
